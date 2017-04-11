8c818493bf74d8e6f93e55c4913465189d6396b7
liulei <xapi@tongwenge.com>
2017-04-11 22:58:16 +0800

    update

--- src/Xapi/Xapi.php
+++ src/Xapi/Xapi.php
@@ -58,4 +58,13 @@ class Xapi
         $this->init();
     }
 }
+function classLoader($class)
+{
+        $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+        $file = __DIR__ . DIRECTORY_SEPARATOR .'src'. DIRECTORY_SEPARATOR . $path . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+        }
+}
+spl_autoload_register('classLoader');
 ?>
